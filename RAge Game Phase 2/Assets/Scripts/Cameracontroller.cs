dcbd3558b12a70b9330836da388656ff777917e1
Neil van der merwe <necktro101@gmail.com>
2018-09-17 11:20:08 +0200

    fixed git push

---
+++ RAge Game Phase 2/Assets/Scripts/Cameracontroller.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cameracontroller : MonoBehaviour {
+
+    public bool lockcursor;
+    public float mousesensitivity = 10f;
+    public Transform target;
+    public float targetdist;
+    public Vector2  pitchminmax = new Vector2 (-40,85);
+
+    public float rotationsmoothtime = .12f;
+    Vector3 rotationsmoothvelocity;
+    Vector3 curentrotation;
+
+    float yaw;
+    float pitch;
+    void Start () {
+        if (lockcursor)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }	
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+
+        yaw += Input.GetAxis("Mouse X") * mousesensitivity;
+        pitch -= Input.GetAxis("Mouse Y") * mousesensitivity;
+        pitch = Mathf.Clamp(pitch, pitchminmax.x, pitchminmax.y);
+
+        curentrotation = Vector3.SmoothDamp(curentrotation, new Vector3(pitch, yaw), ref rotationsmoothvelocity, rotationsmoothtime);
+        transform.eulerAngles = curentrotation;
+
+        transform.position = target.position - transform.forward * targetdist;
+    }
+}
---
+++ RAge Game Phase 2/Assets/Scripts/Spawnmanager.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum Spawnstate
+{
+    wave1 ,
+    wave2 ,
+    wave3 ,
+    stop
+}
+
+public class Spawnmanager : MonoBehaviour {
+    private Spawnstate spawnstate = Spawnstate.wave1;
+    public GameObject[] spawnObject;
+    public Transform [] spawnPosition;
+    public Enemycontoller enemy;
+    public float Timer = 1;
+
+    // Use this for initialization
+    void Start () {
+     
+         Instantiate(spawnObject[Random.Range(0,3)], spawnPosition[Random.Range(0, 4)].position, spawnObject[Random.Range(0, 3)].transform.rotation);
+         enemy.enemycount = 1;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (enemy.shouldspawn==false)
+        {
+            spawnstate = Spawnstate.stop;
+        }
+        else if (enemy.shouldspawn==true && enemy.times==1 )
+        {
+            spawnstate = Spawnstate.wave2;
+        }
+        else if (enemy.shouldspawn == true && enemy.times == 2)
+        {
+            spawnstate = Spawnstate.wave3;
+        }
+        else if (spawnstate == Spawnstate.wave1)
+        {
+            Timer -= Time.deltaTime;
+            if (Timer <= 0)
+            {
+                Wave1spawn();
+                Timer = 4f;
+            }
+        }
+
+        else if (spawnstate == Spawnstate.wave2)
+        {        
+            Timer -= Time.deltaTime;
+            if (Timer <= 0)
+            {
+                Wave2spawn();
+                Timer = 2f;
+            }
+        }
+
+        else if (spawnstate == Spawnstate.wave3)
+        {
+            
+            Timer -= Time.deltaTime;
+            if (Timer <= 0)
+            {
+                Wave1spawn();
+                Timer = 1f;
+            }
+        }
+       
+
+
+    }
+
+    void Wave1spawn()
+    {
+        Instantiate(spawnObject[Random.Range(0, 3)], spawnPosition[Random.Range(0, 4)].position, spawnObject[Random.Range(0, 3)].transform.rotation);
+        enemy.enemycount += 1;
+        
+    }
+
+    void Wave2spawn()
+    {
+        Instantiate(spawnObject[Random.Range(0, 3)], spawnPosition[Random.Range(0, 4)].position, spawnObject[Random.Range(0, 3)].transform.rotation);
+        enemy.enemycount += 1;
+    }
+
+    void Wave3spawn()
+    {
+        Instantiate(spawnObject[Random.Range(0, 3)], spawnPosition[Random.Range(0, 4)].position, spawnObject[Random.Range(0, 3)].transform.rotation);
+        enemy.enemycount += 1;
+    }
+    
+    
+    
+
+
+
+}
