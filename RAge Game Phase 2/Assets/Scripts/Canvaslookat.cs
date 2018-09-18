89147af029c309d1fdbc7359d31223006c44708e
Neil van der merwe <necktro101@gmail.com>
2018-09-18 16:02:20 +0200

    made script for canvas to look at camera

---
+++ RAge Game Phase 2/Assets/Scripts/Canvaslookat.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Canvaslookat : MonoBehaviour {
+    public GameObject cam;
+    public GameObject target;
+
+    // Use this for initialization
+    void Start () {
+        target = GameObject.FindGameObjectWithTag("Player");
+    }
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        
+        transform.LookAt(target.transform);
+
+    }
+   
+        
+}
