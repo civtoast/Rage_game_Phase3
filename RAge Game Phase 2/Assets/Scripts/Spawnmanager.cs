0f879a7258d644f86e94362474e95cab90947e44
DJ-1999 <41787539+DJ-1999@users.noreply.github.com>
2018-10-03 12:44:16 +0200

    Made spike spawn fixed jump and attack

--- RAge Game Phase 2/Assets/Scripts/Spawnmanager.cs
+++ RAge Game Phase 2/Assets/Scripts/Spawnmanager.cs
@@ -17,86 +17,34 @@ public class Spawnmanager : MonoBehaviour {
     public Transform [] spawnPosition;
     public Enemycontoller enemy;
     public float Timer = 1;
-
+    public static List<Enemy> enemywave = new List<Enemy>();
     // Use this for initialization
     void Start () {
      
-         Instantiate(spawnObject[Random.Range(0,3)], spawnPosition[Random.Range(0, 4)].position, spawnObject[Random.Range(0, 3)].transform.rotation);
-         enemy.enemycount = 1;
+        
+         
     }
 	
 	// Update is called once per frame
 	void Update () {
 
-        if (enemy.shouldspawn==false)
+        
+        if (enemywave.Count == 0)
         {
-            spawnstate = Spawnstate.stop;
-        }
-        else if (enemy.shouldspawn==true && enemy.times==1 )
-        {
-            spawnstate = Spawnstate.wave2;
-        }
-        else if (enemy.shouldspawn == true && enemy.times == 2)
-        {
-            spawnstate = Spawnstate.wave3;
-        }
-        else if (spawnstate == Spawnstate.wave1)
-        {
-            Timer -= Time.deltaTime;
-            if (Timer <= 0)
-            {
-                Wave1spawn();
-                Timer = 4f;
-            }
-        }
-
-        else if (spawnstate == Spawnstate.wave2)
-        {        
-            Timer -= Time.deltaTime;
-            if (Timer <= 0)
-            {
-                Wave2spawn();
-                Timer = 2f;
-            }
-        }
-
-        else if (spawnstate == Spawnstate.wave3)
-        {
-            
-            Timer -= Time.deltaTime;
-            if (Timer <= 0)
-            {
-                Wave1spawn();
-                Timer = 1f;
-            }
+            StartCoroutine(Spawmwave(3));
         }
        
 
 
     }
-
-    void Wave1spawn()
+    IEnumerator Spawmwave(int enemycounter)
     {
-        Instantiate(spawnObject[Random.Range(0, 3)], spawnPosition[Random.Range(0, 4)].position, spawnObject[Random.Range(0, 3)].transform.rotation);
-        enemy.enemycount += 1;
-        
+        for (int i =0; i < enemycounter; i++)
+        {
+            GameObject obj = Instantiate(spawnObject[Random.Range(0, 3)], spawnPosition[Random.Range(0, 4)].position, spawnObject[Random.Range(0, 3)].transform.rotation);
+            enemywave.Add(obj.GetComponent<Enemy>());
+            yield return new WaitForSecondsRealtime(Random.Range(1f, 4f));
+
+        }
     }
-
-    void Wave2spawn()
-    {
-        Instantiate(spawnObject[Random.Range(0, 3)], spawnPosition[Random.Range(0, 4)].position, spawnObject[Random.Range(0, 3)].transform.rotation);
-        enemy.enemycount += 1;
-    }
-
-    void Wave3spawn()
-    {
-        Instantiate(spawnObject[Random.Range(0, 3)], spawnPosition[Random.Range(0, 4)].position, spawnObject[Random.Range(0, 3)].transform.rotation);
-        enemy.enemycount += 1;
-    }
-    
-    
-    
-
-
-
 }
---
+++ RAge Game Phase 2/Assets/Scripts/Spikekill.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikekill : MonoBehaviour {
+
+    public void Kill()
+    {
+        Destroy(this.gameObject);
+    }
+}
